3c342c22991d22b8e641d34d3aecc85d1fa6b692
Azzam-sd <idps.0909@gmail.com>
2022-07-28 15:30:03 +0300

    styling the buttons using flex.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,42 +1,59 @@
-import { TextInput, View, StyleSheet, } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return(
-        <View style={styles.InputContainer}>
-            <TextInput style={styles.NumberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false}/>
-            <PrimaryButton>Rest</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+  return (
+    <View style={styles.InputContainer}>
+      <TextInput
+        style={styles.NumberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+      <PrimaryButton>Rest</PrimaryButton>
         </View>
-    );
+        <View style={styles.buttonContainer}>
+      <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
 }
-
 
 export default StartGameScreen;
 
-
 const styles = StyleSheet.create({
-    InputContainer:{
+  InputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
-    padding:16,
+    padding: 16,
     backgroundColor: "#72063c",
     borderRadius: 8,
     shadowColor: "black",
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.3,
-    },
-    NumberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: "white",
-        borderBottomWidth: 2,
-        color:"white",
-        marginVertical: 8,
-        fontWeight: "bold",
-        textAlign: "center",
-
-    },
-})
+  },
+  NumberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "white",
+    borderBottomWidth: 2,
+    color: "white",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex:1,
+  },
+});
