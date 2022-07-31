01c3d440c7aa8b5eb836368fda666a101d7a73a7
Azzam-sd <idps.0909@gmail.com>
2022-07-31 22:22:13 +0300

    Add a new page to contain the number guessed.

---
+++ components/game/NumberContainer.js
@@ -0,0 +1,31 @@
+import {Text, View, StyleSheet} from 'react-native';
+
+
+import { Colors } from '../../constants/colors.js';
+
+
+function NumberContainer({children}){
+    return <View style={styles.container}>
+        <Text  style={styles.numberText}>{children}</Text>
+    </View>
+}
+
+export default NumberContainer;
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 2,
+        borderColor: 'white',
+        padding: 24,
+        margin:24,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+    },
+    numberText:{
+        color: 'white',
+        fontSize: 36, 
+        fontWeight: 'bold',
+    },
+
+})
