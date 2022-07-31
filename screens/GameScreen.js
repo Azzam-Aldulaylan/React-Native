ba7d272925b55e8d07cc82a45c115d130341202a
Azzam-sd <idps.0909@gmail.com>
2022-07-31 22:25:05 +0300

    Add the guessing number mechanism using if statement to narrow the guessed number via the + and - buttons.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,33 +1,87 @@
-import {Text,View,StyleSheet, SafeAreaView} from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, Alert } from "react-native";
+import { useState } from "react";
 
-function GameScreen () {
-    return (
-    <SafeAreaView style={styles.screen}>
-        <Text style={styles.title}>Opponent's Guess</Text>
-        {/* Guess */}
-        <View>
-            <Text>Higher or Lower?</Text>
-            {/* + - */}
-        </View>
-        {/* <View>LOG ROUNDS</View> */}
-    </SafeAreaView>
+import Title from "../components/UI/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/UI/PrimaryButton";
+
+function generateRandomRangedNum(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomRangedNum(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomRangedNum(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === "Lower" && currentGuess < userNumber) ||
+      (direction === "Greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert(
+        "Invalid Input!",
+        "The input is invalid. due to a cheating attempt...",
+        [{ text: "Sorry!", style: "cancel" }]
+      );
+      return;
+    }
+    if (direction === "Lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomRangedNum(
+      minBoundary,
+      maxBoundary,
+      currentGuess
     );
+    setCurrentGuess(newRndNumber);
+  }
+  return (
+    <SafeAreaView style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or Lower?</Text>
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "Grater")}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "Lower")}>
+            -
+          </PrimaryButton>
+        </View>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </SafeAreaView>
+  );
 }
 
 export default GameScreen;
 
 const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-        padding:24,
-    },
-    title: {
-        fontSize:24,
-        fontWeight: 'bold',
-        color: 'white',
-        textAlign: 'center',
-        borderWidth: 1,
-        borderColor: 'white',
-        padding: 12,
-    },
-})
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    borderWidth: 1,
+    borderColor: "white",
+    padding: 12,
+  },
+});
