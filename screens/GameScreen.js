6ad45a8d810ce95034435b4cf0b37bde671f9a84
Azzam-sd <idps.0909@gmail.com>
2022-08-01 10:59:24 +0300

    Replace - and + with Icons using Ionicons from expo.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,6 @@
 import { View, StyleSheet, SafeAreaView, Alert } from "react-native";
 import { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 import Title from "../components/UI/Title";
 import NumberContainer from "../components/game/NumberContainer";
@@ -65,12 +66,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
           <PrimaryButton onPress={nextGuessHandler.bind(this, "Grater")}>
-            +
+          <Ionicons name="md-add" size={24}/>
           </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
           <PrimaryButton onPress={nextGuessHandler.bind(this, "Lower")}>
-            -
+          <Ionicons name="md-remove" size={24}/>
           </PrimaryButton>
           </View>
         </View>
