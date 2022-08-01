f5926e08b19f388aa43aa3863b99eaa9d732f3c9
Azzam-sd <idps.0909@gmail.com>
2022-08-01 17:19:18 +0300

    Apply the game over image and style it.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,39 @@
-import {Text, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, Image } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import Title from "../components/UI/Title";
 
 function GameOverScreen() {
-    return(
-<Text>Game Over</Text>
-
-    );
+  return (
+    <View style={styles.rootContaner}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../assets/images/success.png")} />
+      </View>
+      <Text>Round reached was x to guess number y.</Text>
+    </View>
+  );
 }
 
-export default GameOverScreen;
+export default GameOverScreen; 
 
 const styles = StyleSheet.create({
-
+    rootContaner:{
+    flex: 1,
+    padding:24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primaryW1,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
 });
