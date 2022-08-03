363db24cd45d883dc8606aa150cf1fd5f1dbeb42
Azzam-sd <idps.0909@gmail.com>
2022-08-03 13:27:32 +0300

    Implementing navigation using React Navigation.

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import CategoriesScreens from "./screens/CategoriesScreens";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return ( 
   <>  
-  <SafeAreaView>
-  <StatusBar style='light' />
-  <CategoriesScreens />
-  </SafeAreaView>
+  <StatusBar style='dark' />
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="MealsCategories" component={CategoriesScreens}/>
+    </Stack.Navigator>
+  </NavigationContainer>
 </> 
   );
 }
