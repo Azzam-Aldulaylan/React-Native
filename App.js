e0a365d1e7a6e6c136cb7d6ec2a4e9c5a937704e
Azzam-sd <idps.0909@gmail.com>
2022-08-08 11:09:17 +0300

    Facing an error inplementing the react native reanimated. Will be back to this project later.

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreens from "./screens/CategoriesScreens";
 import MealsScreen from "./screens/MealsScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator(){
+ return <DrawerNavigator>
+  <DrawerNavigator.Screen/>
+ </DrawerNavigator>;
+}
 
 export default function App() {
   return (
@@ -23,22 +31,13 @@ export default function App() {
         >
           <Stack.Screen
             name="Meals Categories"
-            component={CategoriesScreens}
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              //title: "All Categories",
             }}
           />
-          <Stack.Screen
-            name="MealsScreen"
-            component={MealsScreen}
-            // options={({ route, navigation }) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId,
-            //   };
-            // }}
-          />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen}/>
+          <Stack.Screen name="MealsScreen" component={MealsScreen} />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
